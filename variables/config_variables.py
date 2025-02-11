3b2ff2643bf4b692414b00bbf5cdbca5cda2dc8e
Antti Valo <antti.valo@siili.com>
2025-02-11 08:46:26 +0200

    Remove config_variables.py from tracking

--- variables/config_variables.py
+++
@@ -1,8 +0,0 @@
-# Device config variables
-ANDROID_VERSION_LOCAL_1 = '15'
-DEVICE_NAME_LOCAL_1 = 'Medium_Phone_API_35'
-
-# Mock server config variables
-CONFIG_MOCK_SERVER_PATH = ''
-CONFIG_MOCK_SERVER_DOMAIN = '10.0.2.2'
-CONFIG_MOCK_SERVER_PORT = '3000'
